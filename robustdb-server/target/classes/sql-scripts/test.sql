738d428e3e8dcd815e11d1fd374cc8190d56f6eb
jbiao <435666469@qq.com>
2021-01-15 22:05:56 +0800

    add git ignore and test script

---
+++ robustdb-server/target/classes/sql-scripts/test.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Persons ( PersonID int(8) PRIMARY KEY, LastName varchar(255) not null, FirstName varchar(255), Address varchar(255), City varchar(255) );
+
+ALTER table Persons ADD index idx_city(City);
+
+INSERT into Persons(PersonID,LastName,FirstName,Address,City) values (1,'Guo','Jianbiao','Sunqiao','Shanghai');
+
+SELECT LastName, FirstName, Address, City FROM Persons a where City='Shanghai';
+
